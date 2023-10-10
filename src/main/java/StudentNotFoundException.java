ce440f7f761466482b52e5afe22745b17d6226d4
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-10 11:54:47 +0200

    Create StudentNotFoundException class

---
+++ src/main/java/StudentNotFoundException.java
@@ -0,0 +1,9 @@
+public class StudentNotFoundException extends RuntimeException{
+    public StudentNotFoundException() {
+        super("Student not found");
+    }
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
